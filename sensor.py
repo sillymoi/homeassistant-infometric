858a8f55b047784c4293f9dbc3534c47fb8b4426
root <root@core-configurator>
2021-10-01 18:55:50 +0200

    increase timeout to 60

--- sensor.py
+++ sensor.py
@@ -190,7 +190,7 @@ async def get_coordinator(
         try:
             # Note: asyncio.TimeoutError and aiohttp.ClientError are already
             # handled by the data update coordinator.
-            async with async_timeout.timeout(10):
+            async with async_timeout.timeout(60):
                 client = InfometricClient(
                     url,
                     username,
